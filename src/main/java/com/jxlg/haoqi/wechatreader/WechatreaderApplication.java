00e5d881e522bc26e1930a1c26fc4a450ef463d5
xuxunjian <haoqijian@outlook.com>
2018-04-28 15:02:52 +0800

    权限管理dao

--- src/main/java/com/jxlg/haoqi/wechatreader/WechatreaderApplication.java
+++ src/main/java/com/jxlg/haoqi/wechatreader/WechatreaderApplication.java
@@ -1,12 +1,22 @@
 package com.jxlg.haoqi.wechatreader;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan("com.jxlg.haoqi.wechatreader.dao")
 public class WechatreaderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WechatreaderApplication.class, args);
-	}
+    private final static Logger logger = LoggerFactory.getLogger(WechatreaderApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatreaderApplication.class, args);
+        logger.info("logback 成功了");
+        logger.error("logback 成功了");
+        logger.debug("logback 成功了");
+    }
 }
